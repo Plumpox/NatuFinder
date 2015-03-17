0d7cdac82442eb1002862f863caf2634216a9b4d
Plumpox <nougatsan@gmail.com>
2015-03-17 13:51:46 +0100

    Projet Unity

---
+++ Assets/Compteur.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compteur : MonoBehaviour {
+
+	private int compteur = 0;
+	private bool voiceOnce = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (compteur == 1 && !voiceOnce) {
+			audio.Play (); 
+			voiceOnce = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "natu") {
+			compteur++; // compteur = compteur + 1; compteur += 1;
+			other.GetComponentInChildren<Collider>().enabled = false;
+			}
+		if (other.name == "xatu" && compteur == 3) {
+			Debug.Log ("xatu"); 
+			Application.LoadLevel ("Phase1");
+		}
+	}
+}
